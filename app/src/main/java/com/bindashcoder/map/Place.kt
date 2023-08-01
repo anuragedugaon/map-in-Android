3246254bbf17ed918a48b271918130e71f03571b
Anurag <anuragkumar.edugaon@gmail.com>
2023-08-01 16:06:09 +0530

    maping path

---
+++ app/src/main/java/com/bindashcoder/map/Place.kt
@@ -0,0 +1,11 @@
+package com.bindashcoder.map
+
+import com.google.android.gms.maps.model.LatLng
+
+
+data class Place(
+    val name: String,
+    val latLng: LatLng,
+    val address: LatLng,
+    val rating: Float
+)
